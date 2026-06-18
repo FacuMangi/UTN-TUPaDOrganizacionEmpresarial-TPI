a914ab301831cac6470f4209c90ab23f00ed0f9b
Facundo Mangioni <facumangio@gmail.com>
2026-06-18 14:03:33 -0300

    Creada funcion en main y comletado readme

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from funciones import *
 
-try:
-	legajo = validarInputNumero("Bienvenido. Ingrese su legajo: ")
-	
-	empleado = verificarLegajo(legajo, cargarEmpleados())
-	
-	print("Empleado encontrado.")
-	
-	pedirDias = validarInputNumero(f"Cuantos dias se va a solicitar?\nIntroducir dias: ")
-	
-	if verificarSaldo(empleado, pedirDias):
-		if validarInputTexto("Supervisor aprueba? (s/n)").lower() == "s":
-			print(f"RRHH registro su ausencia.\nSu solicitud fue aprobada")
-		else: print("Su solicitud fue rechazada.")
+def main():
+	try:
+		legajo = validarInputNumero("Bienvenido. Ingrese su legajo: ")
+		
+		empleado = verificarLegajo(legajo, cargarEmpleados())
+		
+		print("Empleado encontrado.")
+		
+		pedirDias = validarInputNumero(f"Cuantos desea a solicitar?\nIntroducir dias: ")
+		
+		if verificarSaldo(empleado, pedirDias):
+			if validarInputTexto("Supervisor aprueba? (s/n)").lower() == "s":
+				print(f"RRHH esta registrando su ausencia.\nSu solicitud fue aprobada")
+			else: print("Su solicitud fue rechazada.")
 
-except Exception as e:
-	print(e)
+	except Exception as e:
+		print(e)
